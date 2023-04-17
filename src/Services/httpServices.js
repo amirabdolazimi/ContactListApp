2677691ac11661424162b279028d8faa7c525aff
amirabdolazimi <amirabdolazimi.v@gmail.com>
2023-04-17 07:07:50 +0430

    online server created and used to instead of local json-server

--- src/Services/httpServices.js
+++ src/Services/httpServices.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL =
+  "https://interesting-archimedes-4x-peeezt.iran.liara.run";
 const http = {
   get: axios.get,
   post: axios.post,
